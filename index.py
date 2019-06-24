73022a641151d28548e998c3ff223a0afb0f5eaa
Will Scargill <willscargill@outlook.com>
2019-06-24 14:30:11 +0100

    Update gitignore and added index.py base

---
+++ index.py
@@ -0,0 +1,22 @@
+import discord
+import aiohttp
+
+f = open("token.txt", "r")
+token = f.read()
+print(token)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(token)
